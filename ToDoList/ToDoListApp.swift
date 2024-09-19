0713d9ec6fa4f5bc6f29ddca456b9654d02a299f
Gustavo Monge <egnom.ai@gmail.com>
2024-09-19 12:58:14 -0600

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Gustavo Monge on 9/19/24.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
